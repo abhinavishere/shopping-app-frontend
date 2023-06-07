de06b531e7b92062f8675d95f79593e8fb96dea1
Abhinav-Sawarkar <Abhinav.Sawarkar@ibm.com>
2023-06-07 18:02:17 +0530

    Added Chakra UI and themes and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 function App() {
-  return <div>Shoppingify Frontend</div>;
+  return (
+    <>
+      {/* Sidenav
+        //  Logo
+        //  Tabs
+        //  Cart Button (Toggle)
+      */}
+      {/* Main
+          // Header at top (Title + Search form to filter products)
+          // Products List at bottom
+      */}
+      {/* Aside
+        //  User Profile 
+        //  Product details
+        //  New Item Form
+        // Shopping List
+      */}
+    </>
+  );
 }
 
 export default App;
---
+++ src/utils/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    primary: "#F9A109",
+    secondary: "#333333",
+    tertiary: "#454545",
+    primaryLight: "#FFF0DE",
+    cardBg: "#80485B",
+  },
+  fonts: {
+    body: "Quicksand, sans-serif",
+    heading: "Quicksand, sans-serif",
+  },
+});
+
+export default theme;
